a0e01eb817ec3ede2cbc7601e5c2414c94111f6f
Mireiaat92 <mimi.at92@gmail.com>
2016-03-26 21:05:21 +0100

    Login & Register

---
+++ app/src/androidTest/java/tfg2016/gymapp_tfg/ApplicationTest.java
@@ -0,0 +1,13 @@
+package tfg2016.gymapp_tfg;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
